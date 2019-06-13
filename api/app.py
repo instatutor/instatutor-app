9bdb090b80f5d3ceef4d68556cdb39f691569603
Khalid Talakshi <khalid.talakshi@outlook.com>
2019-06-13 10:06:59 -0400

    Created better error handling code

--- api/app.py
+++ api/app.py
@@ -22,8 +22,7 @@ def register():
     currentDb = connectToDatabase()
     regResult = createUser(requestData, currentDb)
     closeConnection(currentDb)
-    
-    return Response(json.dumps(regResult), mimetype='applicaton/json') 
+    return regResult
 
 @app.route('/api/login', methods = ['POST'])
 def login():
@@ -53,12 +52,13 @@ def createUser(userDetails, database):
     sql = "INSERT INTO `Users` (`id`, `first-name`, `last-name`, `email`, `password`, `role`, `points`) VALUES  ('', %s, %s, %s, %s, %s, 0)"
     val = (firstName, lastName, email, password, role)
     mycursor = database.cursor(dictionary=True, buffered=True)
-    doesUserExist = mycursor.execute(checkUser)
+    mycursor.execute(checkUser)
+    doesUserExist = mycursor.fetchone()
     if (doesUserExist == None):
         mycursor.execute(sql, val)
         database.commit()
         mycursor.close()
-        return json.dumps(userDetails)
+        return Response(json.dumps(userDetails), mimetype='application/json')
     else: 
         return "User already exists"   
 
@@ -66,6 +66,8 @@ def loginUser(email, password, database):
     mycursor = database.cursor(dictionary=True)
     mycursor.execute("SELECT * FROM Users WHERE email='" + email+ "'")
     myresult = mycursor.fetchone()
+    if (myresult == None):
+        return "Error-User not found"
     print(myresult)
     if (sha256_crypt.verify(password, myresult['password'])):
         return myresult
